adb8a1feabe0ba6302a50aa36a7e4f4344cd4f78
Dusty McCord and Nick Gallo <nick@>
2020-01-22 16:19:06 -0800

    add simple palindrome function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
 
+ function checkPalindrome(pal) {
+   var palindromes = pal.replace(/ /g,'');
+   var reversed = pal.replace(/ /g,'').split('').reverse().join('');
+    
+   if(reversed == palindromes){
+     console.log("true");
+   } else {
+     console.log("false");
+   }
+ }
+
+ 
+
+  $('button').click(function(event){
+    event.preventDefault();
+    var input = $('input').val();
+    checkPalindrome(input)
+  });
+
 });
